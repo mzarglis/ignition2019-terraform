65704cf6881f69f4ce78866f3cd2cbb6aef74fa3
Michael Zarglis <994599+mzarglis@users.noreply.github.com>
2019-03-01 13:39:49 -0800

    Added client and vpn gateway

--- ignition2019lab.tf
+++ ignition2019lab.tf
@@ -44,7 +44,7 @@ resource "azurerm_network_security_group" "Default_NSG" {
 # Create virtual network
 resource "azurerm_virtual_network" "GOTnet" {
     name                = "GOTnet"
-    address_space       = ["10.1.0.0/16"]
+    address_space       = ["10.0.0.0/15"]
     location            = "${var.region}"
     resource_group_name = "${azurerm_resource_group.igition2019lab.name}"
 
@@ -55,7 +55,7 @@ resource "azurerm_virtual_network" "GOTnet" {
 
 resource "azurerm_virtual_network" "GOTnet2" {
     name                = "GOTnet2"
-    address_space       = ["10.0.0.0/14"]
+    address_space       = ["10.2.0.0/15"]
     location            = "${var.region}"
     resource_group_name = "${azurerm_resource_group.igition2019lab.name}"
 
@@ -88,6 +88,75 @@ resource "azurerm_subnet" "NorthVale" {
     address_prefix       = "10.3.0.0/24"
 }
 
+#####################################
+# Virtual Gateway Network
+#####################################
+resource "azurerm_subnet" "GatewaySubnet" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = "${azurerm_resource_group.igition2019lab.name}"
+  virtual_network_name = "${azurerm_virtual_network.GOTnet2.name}"
+  address_prefix       = "10.2.254.0/24"
+}
+resource "azurerm_public_ip" "GOTnet2-VPN-pub" {
+  name                = "GOTnet2-VPN-pub"
+  location            = "${var.region}"
+  resource_group_name = "${azurerm_resource_group.igition2019lab.name}"
+
+  allocation_method = "Dynamic"
+}
+
+resource "azurerm_virtual_network_gateway" "GOTnet2-VPN" {
+  name                = "GOTnet2-VPN"
+  location            = "${var.region}"
+  resource_group_name = "${azurerm_resource_group.igition2019lab.name}"
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  active_active = false
+  enable_bgp    = false
+  sku           = "VpnGw1"
+
+  ip_configuration {
+    name                          = "vnetGatewayConfig"
+    public_ip_address_id          = "${azurerm_public_ip.GOTnet2-VPN-pub.id}"
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = "${azurerm_subnet.GatewaySubnet.id}"
+  }
+
+  vpn_client_configuration {
+    address_space = ["172.16.0.0/24"]
+    vpn_client_protocols = ["SSTP","IkeV2"]
+
+    root_certificate {
+      name = "ETS-RootCert.pfx"
+
+      public_cert_data = <<EOF
+MIIDAjCCAeqgAwIBAgIQEm3CBOo4UptAZK6tTOQdfzANBgkqhkiG9w0BAQsFADAX
+MRUwEwYDVQQDDAxFVFMtUm9vdENlcnQwHhcNMTkwMzAxMTkyOTI0WhcNMjAwMzAx
+MTk0OTI0WjAXMRUwEwYDVQQDDAxFVFMtUm9vdENlcnQwggEiMA0GCSqGSIb3DQEB
+AQUAA4IBDwAwggEKAoIBAQCW4Xlm4a4+/vh+hxbfa61OOZHmIRZ2h0B1quTDhlY8
+DQVVZnrZjF55odU4vhyu7EDLWIgsFPX7YCkXlPMsKFk00Wx+hTEqepfT04a2yP3Z
+rhutQRUmQMk+OJ3ZYFk838JUyAWDp0fig7kjsyV8b4OGIoUvHyBa9JKHbg2szCGy
+6Xi7ciuqzBLRwikNHR43Exvg93ER5W6Mn/B5hrZ1+83gb5exnQJ3qii4sKDE98Ib
+KL3FizFx2fELUZJ/HoBxK8NPOCyxPcIl1agMqz40QyyEKpnK9npJYXffNUPCPkn4
+etM0xQREziIOtuFVZQGKlPv/jD9HmUEXFKB5RlpmK2KtAgMBAAGjSjBIMA4GA1Ud
+DwEB/wQEAwICBDAXBgNVHREEEDAOggxFVFMtUm9vdENlcnQwHQYDVR0OBBYEFFJf
+Zp5hwf+bZiwfjfuBROrObBJkMA0GCSqGSIb3DQEBCwUAA4IBAQBJTDLlqqUP6c1P
+CsE0g/6wiIyQJ8v5HgdFbGhi+lDIt9WfC7eYUIM/gvjoor4NkjSjngLA1vbi4T4X
+81/ub17wyqT3ExA5tqD121GQxIRnWDCc4yWqSlMsYkn0bkBmJdu+J8dNxCYgnLU0
+5pGwku2sJXzK06OzCTR8jWHOAT4Wfgvq5Oq8MTRl14gSV2v1IGUra0a7IMpwGEdE
+au/TlAB9Zwtcjwy5GXKqtBFjSTDedFmxwSF1HR/EX2GauA4a94okd/an0ZfG1OJw
+UYz/RGUKVs1G0PkJW/fEi26L+rDpPYoEOIeC9KC3J7s5EQHvGm91Vn2VR00mKxrK
+BOrqRQZD
+EOF
+    }
+  }
+}
+
+
+
+
 # Create network interface
 resource "azurerm_network_interface" "DC_Highgarden_Nic" {
     name                      = "DC_Highgarden_nic"
@@ -114,6 +183,7 @@ resource "azurerm_network_interface" "Client_Highgarden_Nic" {
     name                      = "Client_Highgarden_nic"
     location                  = "${var.region}"
     resource_group_name       = "${azurerm_resource_group.igition2019lab.name}"
+    network_security_group_id = "${azurerm_network_security_group.Default_NSG.id}"
 
     ip_configuration {
         name                          = "myNicConfiguration"
@@ -274,6 +344,28 @@ resource "azurerm_network_interface" "SCCM_TheSevenKingdoms_Nic" {
 }
 resource "azurerm_public_ip" "SCCM_TheSevenKingdoms_pub" {
     name                         = "SCCM_TheSevenKingdoms_pub"
+    location                     = "${var.region}"
+    resource_group_name          = "${azurerm_resource_group.igition2019lab.name}"
+    allocation_method            = "Static"
+}
+
+resource "azurerm_network_interface" "Client_TheSevenKingdoms_Nic" {
+    name                      = "Client_TheSevenKingdoms_Nic"
+    location                  = "${var.region}"
+    resource_group_name       = "${azurerm_resource_group.igition2019lab.name}"
+    network_security_group_id = "${azurerm_network_security_group.Default_NSG.id}"
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = "${azurerm_subnet.TheSevenKingdoms.id}"
+        private_ip_address_allocation = "static"
+        private_ip_address            = "10.2.0.254"
+        public_ip_address_id          = "${azurerm_public_ip.Client_TheSevenKingdoms_pub.id}"
+    }
+}
+
+resource "azurerm_public_ip" "Client_TheSevenKingdoms_pub" {
+    name                         = "Client_TheSevenKingdoms_pub"
     location                     = "${var.region}"
     resource_group_name          = "${azurerm_resource_group.igition2019lab.name}"
     allocation_method            = "Static"
@@ -578,6 +670,40 @@ resource "azurerm_virtual_machine" "SCCM_TheSevenKingdoms" {
   os_profile {
 
     computer_name      = "SCCM"
+    admin_username     = "ignition"
+    admin_password     = "Ignition2019"
+
+  }
+
+  os_profile_windows_config {
+  }
+
+}
+
+resource "azurerm_virtual_machine" "Client_TheSevenKingdoms" {
+    name                          = "Client_TheSevenKingdoms"
+    location                      = "${var.region}"
+    resource_group_name           = "${azurerm_resource_group.igition2019lab.name}"
+    network_interface_ids         = ["${azurerm_network_interface.Client_TheSevenKingdoms_Nic.id}"]
+    vm_size                       = "${var.small_instance}"
+    delete_os_disk_on_termination = "false"
+
+    storage_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "Windows-10"
+    sku       = "rs5-pro"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "Client_TheSevenKingdoms_disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name      = "ClientSeven"
     admin_username     = "ignition"
     admin_password     = "Ignition2019"
 
